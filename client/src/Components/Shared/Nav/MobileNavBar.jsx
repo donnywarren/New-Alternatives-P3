714bfab266a851b8818e16aca0bd7a5bd92923d1
donnywarren9 <donnywarren9@gmail.com>
2020-11-02 20:46:23 -0500

    add drop down screen and functionality to hamburger

--- client/src/Components/Shared/Nav/MobileNavBar.jsx
+++ client/src/Components/Shared/Nav/MobileNavBar.jsx
@@ -1,15 +1,33 @@
-import React from "react"
+import React, {useState} from "react"
 import "./MobileNavBar.css"
 
 const MobileNavBar = () => {
+  const [displayVisible, updateDisplayVisible] = useState("")
+
+
+  const handleClick = () => {
+    displayVisible === "" ?
+      updateDisplayVisible("drop-down-container-display") :
+      updateDisplayVisible("")
+  }
+
   return (
     <div className=".nav-bar">
-      <a className="donate"
-        href="https://fundraise.newalternativesnyc.org/give/157653/#!/donation/checkout"
-        target="_blank"
-      >
-        Donate
-      </a>
+      <div className="position-container">
+        <a className="donate"
+          href="https://fundraise.newalternativesnyc.org/give/157653/#!/donation/checkout"
+          target="_blank"
+        >
+          Donate
+        </a>
+        <div className="hamburger" onClick={handleClick}>
+          <div className="black-bar top"></div>
+          <div className="black-bar middle"></div>
+          <div className="black-bar bottom"></div>
+        </div>
+        <div className={`drop-down-container ${displayVisible}`}>
+        </div>
+      </div>
     </div>
   )
 }
