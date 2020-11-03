7110e873d5fd6465a34ea77a0908a89dc855b24d
donnywarren9 <donnywarren9@gmail.com>
2020-11-02 19:33:04 -0500

    add window listener for mobile size, add MobileNavBar component

---
+++ client/src/Components/Shared/Nav/MobileNavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "./MobileNavBar.css"
+
+const MobileNavBar = () => {
+  return (
+    <div className=".nav-bar">
+      <a className="donate"
+        href="https://fundraise.newalternativesnyc.org/give/157653/#!/donation/checkout"
+        target="_blank"
+      >
+        Donate
+      </a>
+    </div>
+  )
+}
+
+
+export default MobileNavBar
--- client/src/Components/Shared/Nav/Nav.jsx
+++ client/src/Components/Shared/Nav/Nav.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 import "./Nav.css";
+import MobileNavBar from "./MobileNavBar"
 import Social from "../../Social/Social";
 import Logo from "../../../Assets/new-alternatives-title.png";
+
 const Nav = () => {
+  const size = window.innerWidth
+  const [screenSize, updateScreenSize] = useState("")
+
+  const handleResize = () => {
+    updateScreenSize(size)
+  }
+  window.addEventListener("resize", handleResize)
+
+
   return (
     <>
       <div className="nav-component">
@@ -14,43 +25,46 @@ const Nav = () => {
             </NavLink>
           </div>
         </div>
-        <div className="nav-bar">
-          <div className="top-nav-bar">
-            <div>
-              <NavLink className="contact-us nav-links" to="/coming-soon">
-                Contact Us
+        {screenSize < 700 ? 
+        
+        <MobileNavBar/> :
+        
+          <div className="nav-bar">
+            <div className="top-nav-bar">
+              <div>
+                <NavLink className="contact-us nav-links" to="/coming-soon">
+                  Contact Us
               </NavLink>
-            </div>
-            <div className="social-media-box">
-              <Social />
-            </div>
-            <button className="donate" alt="donate button">
-              <a
+              </div>
+              <div className="social-media-box">
+                <Social />
+              </div>
+              <a className="donate"
                 href="https://fundraise.newalternativesnyc.org/give/157653/#!/donation/checkout"
                 target="_blank"
               >
                 Donate
               </a>
-            </button>
+            </div>
+            <div className="nav-menu" alt="navigation menu">
+              <NavLink className="nav-links" to="/">
+                Home
+            </NavLink>
+              <NavLink className="nav-links" to="/coming-soon">
+                About Us
+            </NavLink>
+              <NavLink className="nav-links" to="/our-stories">
+                Our Stories
+            </NavLink>
+              <NavLink className="nav-links" to="/coming-soon">
+                Get Help
+            </NavLink>
+              <NavLink className="nav-links" to="/coming-soon">
+                Get Involved
+            </NavLink>
+            </div>
           </div>
-          <div className="nav-menu" alt="navigation menu">
-            <NavLink className="nav-links" to="/">
-              Home
-            </NavLink>
-            <NavLink className="nav-links" to="/coming-soon">
-              About Us
-            </NavLink>
-            <NavLink className="nav-links" to="/our-stories">
-              Our Stories
-            </NavLink>
-            <NavLink className="nav-links" to="/coming-soon">
-              Get Help
-            </NavLink>
-            <NavLink className="nav-links" to="/coming-soon">
-              Get Involved
-            </NavLink>
-          </div>
-        </div>
+        }
       </div>
     </>
   );
