6524f4b61f2998340981b2f4739b9dae94643376
donnywarren9 <donnywarren9@gmail.com>
2020-11-04 12:56:54 -0500

    completed hero mobile adjustments, also smaller purple discs, complete mobile adjustmets to who we help

--- client/src/Components/Hero/Hero.jsx
+++ client/src/Components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 import "./Hero.css"
 
 export default function Hero(props) {
@@ -15,6 +16,9 @@ export default function Hero(props) {
         <div className="shadow-box">
           <h2>New Alternatives for LGBTQ+ Homeless Youth</h2>
           <p>New Alternatives increases the self-sufficiency of LGBTQ+ homeless youth by enabling them to transition out of the shelter system to stable adult lives.</p>
+          <NavLink className="learn-link" to="/coming-soon">
+            {` Learn more >`}
+          </NavLink>
         </div>
        </div>
       </div>
