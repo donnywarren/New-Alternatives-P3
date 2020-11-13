b54734f5b88778e06e1e954760c5b5be3aedafee
donnywarren9 <donnywarren9@gmail.com>
2020-11-12 19:45:02 -0500

    create new images for mobile mission carousel, add conditional rendering for mobile carousel images

--- client/src/Components/MissionCarousel/MissionCarousel.jsx
+++ client/src/Components/MissionCarousel/MissionCarousel.jsx
@@ -1,14 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
 import { CarouselProvider, Slider, Slide, DotGroup } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
 import "./MissionCarousel.css";
 import Educate from "../../Assets/exist-educate.png";
+import EducateText from "../../Assets/exist-educate-text.png"
 import Food from "../../Assets/exist-food.png";
+import FoodText from "../../Assets/exist-food-text.png"
 import Testing from "../../Assets/exist-testing.png";
+import TestingText from "../../Assets/exist-testing-text.png"
 
 function MissionCarousel(props) {
+  const [missionImage, setMissionImage] = useState("")
   const testimonials = props.content;
+
+  useEffect(() => {
+    function handleResize() {
+      window.innerWidth < 700 ? 
+        setMissionImage("mobile") :
+        setMissionImage("")
+    }
+    handleResize()
+  })
 
   return (
     <div className="complete-mission-carousel">
@@ -22,13 +34,21 @@ function MissionCarousel(props) {
       >
         <Slider className="slider-container">
           <Slide>
-            <img className="mission-image" src={Educate} />
+            <img
+              className="mission-image"
+              src={missionImage !== "mobile" ? Educate  : EducateText }
+            />
           </Slide>
           <Slide>
-            <img className="mission-image" src={Food} />
+            <img
+              className="mission-image"
+              src={missionImage !== "mobile" ? Food : FoodText}
+            />
           </Slide>
           <Slide>
-            <img className="mission-image" src={Testing} />
+            <img
+              className="mission-image"
+              src={missionImage !== "mobile" ? Testing : TestingText} />
           </Slide>
         </Slider>
         <DotGroup className="dot-group" />
